c855f870f9451805d98eea96b78f73f759dbbaf6
Karla-gz <karlaguadalupeguzmanvargas4@gmail.com>
2026-02-12 18:26:37 -0800

    Se añadio al documento la deteccion de numeros par e impar

--- sesion_01.py
+++ sesion_01.py
@@ -1,32 +1,68 @@
-# Numericos y Flotantes
+# Numeros
 print(int(7))
 print(float(7.7))
-print(type(7))
-print(type(7.77))
+print (type(7))
+print (type(7.7))
 print(int(1+2))
 print(int(10*2))
-print("==== Operadores Matematicos========")
+print(int(1 + 4 - 2))
+print(float(1 + 2.0))
+
+# Operadores matematicas
 # +
 # -
 # *
 # /
 # **
 # % Modulo
+
 print(int(2**3))
 print(int(4**8))
-print(float(10%2))
+print(float(10%3))
 print(float(25%4))
+print(float(16%2))
+print(float(10 / 3))
 
-ventas = 1999999
+# Variables 
+print("========VARIABLES========")
+x = 100
+y = 1
+print(x + y)
+
+ventas = 199991
 print("Nuestras ventas fueron: ", ventas)
+
 is_active = True
-print(bool(is_active))
+print(is_active)
 
 game_over = False
 print(game_over)
 
-edad = 16 
-if(edad = 16):
-    print("Si puedo entrar a el Bar!")
+some_string = "Hola soy un string"
+print(some_string)
+
+print("==========Condicionales==========")
+edad = 20
+
+if(edad >= 18):
+    print("Si puedes entrar a el Bar!!!")
 else:
-    print("No puedo entrar a el Bar!")
+    print("Si puedes entrar al Bar!!!")
+
+mi_numero = int(input("¿Cual es numero que deseas verificar?:"))
+print(f"El numero que deseas verificar es {mi_numero}")
+if mi_numero % 2 == 0:
+    print(f"El numero {mi_numero} es par!")
+else: 
+    print(f"El numero {mi_numero} es inpar!")
+
+def par_impar(numero):
+    if numero % 2 == 0:
+        print(f"El numero {numero} es par!!!")
+    else:
+        print(f"El numero {numero} es inpar!!")
+
+print("============FUNCION PAR_INPAR()=========")
+mi_numero = int(input("¿Cual es numero que deseas verificar?:"))
+print(f"El numero que deseas verificar es {mi_numero}")
+print(par_impar(mi_numero))
